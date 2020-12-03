aed3c3678fbf97fca002a79348873af90a454c57
raju-ahammad <web.rajuahammad@gmail.com>
2020-12-03 10:40:08 +0600

    create new app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
+const Hello = ({name, age}) => {
+  const bornYear = () =>  new Date().getFullYear() - age
+  return (
+    <div>
+      <p>Hello { name } you are {age} years old</p>
+      
+      <h4>You are probably born in { bornYear() }</h4>
+    </div>
+  )
+}
+
 function App() {
+  const name ="Ahammad"
+  const age = 24
   return (
     <div className="App">
       <header className="App-header">
-        Hello dev
+        <h1>Greetings</h1>
+        <Hello name="Raju" age={10+24}/>
+        <Hello name={name} age={age}/>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const History = (props) => {
+  if (props.allclicks.length === 0) {
+    return  <div>The app is pressing</div>
+  }
+  return <div>
+    button press the history : { props.allclicks.join(" ") }
+  </div>
+}
+
+
+const App = () => {
+
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allclicks, setAllClick] = useState([])
+
+  const handleLeftClick = () => {
+    setAllClick(allclicks.concat("L"))
+   
+    setLeft(left+1)
+  }
+  const handleRightClick = () => {
+    setAllClick(allclicks.concat("R"))
+    setRight(right+1)
+  }
+ 
+  return(
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {right}
+      <div>
+        <p> {allclicks.join(" ")} </p>
+      </div>
+      <History allclicks={ allclicks } />
+    </div>
+  );
+}
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
